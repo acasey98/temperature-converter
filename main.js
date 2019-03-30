b73d65c3969146bd95a229187b62f87c350f6aa0
Austin Casey <thepoast@gmail.com>
2019-03-30 02:14:52 -0500

    finished

--- main.js
+++ main.js
@@ -4,24 +4,52 @@ const printToDom = (divId, textToPrint) => {
 };
 
 const determineConverter = () =>{
+  let domString = '';
+  if (document.querySelector('input[name="temperature"]:checked').value === 'C'){
 
+    let finalTemp = document.getElementById('tempInput').value * 1.8 + 32;
+
+    if (finalTemp <= 32){
+      document.getElementById('tempOutput').style.color = 'blue';
+    }else if (finalTemp > 32 && finalTemp < 90){
+      document.getElementById('tempOutput').style.color = 'green';
+    }else if (finalTemp >= 90){
+      document.getElementById('tempOutput').style.color = 'red';
+    }
+
+    domString += `<h3>${finalTemp} Degrees Fahrenheit</h3>`
+
+  } else if (document.querySelector('input[name="temperature"]:checked').value === 'F'){
     
+    let finalTemp = (document.getElementById('tempInput').value - 32) / 1.8;
+    if (finalTemp <= 0){
+      document.getElementById('tempOutput').style.color = 'blue';
+    }else if (finalTemp > 0 && finalTemp < 32){
+      document.getElementById('tempOutput').style.color = 'green';
+    }else if (finalTemp >= 32){
+      document.getElementById('tempOutput').style.color = 'red';
+    }
+    domString += `<h3>${finalTemp} Degrees Celsius</h3>`
+
+  }
+
+    printToDom('tempOutput', domString);
+};
+
+const clearInput = () =>{
+document.getElementById('tempInput').value = '';
 };
 
 const buttonEvents = () => {
     document.getElementById('convertBtn').addEventListener('click', determineConverter);
-    document.getElementById('tempInput').addEventListener('keydown', determineConverter);
-    document.getElementById('clearBtn').addEventListener('click', );
-    document.getElementById('radio').addEventListener('submit', function(event)
-    {
-        let data = new FormData(form);
-        let output = "";
-        for (const entry of data) {
-          output = entry[1];
-        };
-        console.log(output);
-        event.preventDefault();
-    }, false);
+    document.getElementById('tempInput').addEventListener('keydown', function (e) {
+      var key = e.which || e.keyCode;
+      if (key === 13) { // 13 is enter
+        determineConverter();
+      }
+  });
+    document.getElementById('clearBtn').addEventListener('click', clearInput);
+    
   };
 
 
